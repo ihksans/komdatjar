87e751b8129a3be2f4c5f083ff1a5a8e703c9cb3
ikhsan setiawan <ikhsan.setiawan.tif18@polban.ac.id>
2021-02-07 19:24:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/san47/komdatjar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.san47.komdatjar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "ed70e49b-0fae-47d7-8315-d699d1ea69e1",
+                Analytics.class, Crashes.class);
+    }
+}
